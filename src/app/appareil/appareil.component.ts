045c9607adb56e88bde73115105a8c6200508dbb
Guisharko <guillaume.gallier@sfr.fr>
2019-10-10 22:32:11 +0200

    maj app component

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -9,7 +9,13 @@ export class AppareilComponent implements OnInit {
 
   @Input() appareilName: string;
   @Input() appareilStatus: string;
-
+  getColor() {
+    if (this.appareilStatus === 'allumé') {
+      return 'green';
+    } else if (this.appareilStatus === 'éteint') {
+      return 'red';
+    }
+  }
   constructor() { }
 
   ngOnInit() {
