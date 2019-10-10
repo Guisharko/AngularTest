a422fc0e1e58b898c2e186e3e1040a251fbc3ec7
Guisharko <guillaume.gallier@sfr.fr>
2019-10-10 23:24:48 +0200

    ajout du component blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppareilComponent } from './appareil/appareil.component';
+import { BlogComponent } from './blog/blog.component';
 import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppareilComponent
+    AppareilComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
