bb38be26c249e4b9a8bf2d35ccac3cd1330bd917
Guisharko <guillaume.gallier@sfr.fr>
2019-10-12 01:16:50 +0200

    ajout du component blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,51 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    appareils = [
-        {
-            name: 'Machine à laver',
-            status: 'éteint'
-        },
-        {
-            name: 'Frigo',
-            status: 'allumé'
-        },
-        {
-            name: 'Ordinateur',
-            status: 'éteint'
-        }
-    ];
-
-    posts = [
-        {
-            title: 'Mon premier post',
-            content: 'Dynamically expedite client-centric potentialities through real-time resources. Appropriately ' +
-                'harness highly efficient core.',
-            loveIts: 0,
-            created_at: new Date(),
-        },
-        {
-            title: 'Mon deuxième post',
-            content: 'Dynamically expedite client-centric potentialities through real-time resources. Appropriately ' +
-                'harness highly efficient core.',
-            loveIts: 0,
-            created_at: new Date(),
-        },
-        {
-            title: 'Encore un post',
-            content: 'Dynamically expedite client-centric potentialities through real-time resources. Appropriately ' +
-                'harness highly efficient core.',
-            loveIts: 0,
-            created_at: new Date(),
-        }
-    ];
-
-    constructor() {
+  postCreate = new Date();
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
     }
+  ];
+
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: 'Dynamically expedite client-centric potentialities through real-time resources. Appropriately ' +
+        'harness highly efficient core.',
+      loveIts: 0,
+      created_at: this.postCreate,
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Dynamically expedite client-centric potentialities through real-time resources. Appropriately ' +
+        'harness highly efficient core.',
+      loveIts: 0,
+      created_at: this.postCreate,
+    },
+    {
+      title: 'Encore un post',
+      content: 'Dynamically expedite client-centric potentialities through real-time resources. Appropriately ' +
+        'harness highly efficient core.',
+      loveIts: 0,
+      created_at: this.postCreate,
+    }
+  ];
+
+  constructor() {
+  }
 }
